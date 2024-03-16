d8d64163c44db8a74696d3b872aca712cb765174
George Murphy <321ebikes@gmail.com>
2024-03-16 17:41:51 -0400

    Name spaced document upload components.

---
+++ app/Components/DocumentUploadComponents.tsx
@@ -0,0 +1,13 @@
+"use client"
+export  {default as Title} from './Title';
+export  {default as Close} from './Close';
+export  {default as Conetent} from './Content';
+export  {default as ClientRadio} from './ContentComponents/ClientRadio';
+export  {default as ClientSelector} from './ContentComponents/ClientSelector';
+export  {default as ElapsedCheck} from './ContentComponents/ElapsedCheck';
+export  {default as FileUpload} from './ContentComponents/FileUpload';
+export  {default as InputName} from './ContentComponents/InputName';
+export  {default as Location} from './ContentComponents/Location';
+export  {default as Split} from './ContentComponents/Split';
+export  {default as TolerenceWindow} from './ContentComponents/TolerenceWindow';
+
---
+++ app/Components/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+interface FooterPropTypes{
+
+}
+const Footer: React.FC<FooterPropTypes> = (props) =>{
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Footer.propTypes = {
+
+}
+
+export default Footer
+
