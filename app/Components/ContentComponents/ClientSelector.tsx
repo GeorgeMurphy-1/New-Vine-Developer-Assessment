ae6ff354aca08a75d3e53f42a24a619e8dcfdc05
George Murphy <321ebikes@gmail.com>
2024-03-18 11:25:42 -0400

    adjust space for input in client selector

--- app/Components/ContentComponents/ClientSelector.tsx
+++ app/Components/ContentComponents/ClientSelector.tsx
@@ -1,45 +1,79 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import InputStyled from '../Shared/InputStyled'
-import Data from '../../staticData/Data.json'
-import Image from 'next/image'
-import './cLientSelector.css';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import InputStyled from "../Shared/InputStyled";
+import Data from "../../staticData/Data.json";
+import Image from "next/image";
+import "./cLientSelector.css";
 
 export default class ClientSelector extends Component {
   static propTypes = {
-    prop: PropTypes
-  }
+    prop: PropTypes,
+  };
 
   render() {
     return (
       <div>
-          <div><span>Testing center 1</span> <span className='mlr1em w50'><InputStyled options={Data.options} placeholder={Data['client input']} /></span><Image
-          height="20"
-          width="20"
-          alt="seperator"
-          src={"./wait Icon.svg"}
-        /></div>
- <div><span>Testing center 2</span> <span className=' mlr1em w50'><InputStyled options={Data.options} placeholder={Data['client input']} /></span><Image
-          height="20"
-          width="20"
-          alt="seperator"
-          src={"./wait Icon.svg"}
-        /></div>
-           <div><span>Testing center 3</span> <span className='mlr1em w50'><InputStyled options={Data.options} placeholder={Data['client input']} /></span><Image
-          height="20"
-          width="20"
-          alt="seperator"
-          src={"./wait Icon.svg"}
-        /></div><div><span>Testing center 4</span> <span className=' mlr1em w50'><InputStyled options={Data.options} placeholder={Data['client input']} /></span><Image
-          height="20"
-          width="20"
-          alt="seperator"
-          src={"./wait Icon.svg"}
-        /></div>
-          
-        
-                     
+        <div className="mtb">
+          <span>Testing center 1</span>{" "}
+          <span className="mlr1em w50">
+            <InputStyled
+              options={Data.options}
+              placeholder={Data["client input"]}
+            />
+          </span>
+          <Image
+            height="20"
+            width="20"
+            alt="seperator"
+            src={"./wait Icon.svg"}
+          />
+        </div>
+        <div className="mtb">
+          <span>Testing center 2</span>{" "}
+          <span className=" mlr1em w50">
+            <InputStyled
+              options={Data.options}
+              placeholder={Data["client input"]}
+            />
+          </span>
+          <Image
+            height="20"
+            width="20"
+            alt="seperator"
+            src={"./wait Icon.svg"}
+          />
+        </div>
+        <div className="mtb">
+          <span>Testing center 3</span>{" "}
+          <span className="mlr1em w50">
+            <InputStyled
+              options={Data.options}
+              placeholder={Data["client input"]}
+            />
+          </span>
+          <Image
+            height="20"
+            width="20"
+            alt="seperator"
+            src={"./wait Icon.svg"}
+          />
+        </div>
+        <div className='mtb'>
+          <span>Testing center 4</span>{" "}
+          <span className=" mlr1em w50">
+            <InputStyled
+              options={Data.options}
+              placeholder={Data["client input"]}
+            />
+          </span>
+          <Image
+            height="20"
+            width="20"
+            alt="seperator"
+            src={"./wait Icon.svg"}
+          />
+        </div>
       </div>
-    )
+    );
   }
 }
